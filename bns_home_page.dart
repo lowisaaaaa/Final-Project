5558e36010fa8695e5954424717dd53dfa9ab9f6
lowisaaaaa <118810600+lowisaaaaa@users.noreply.github.com>
2025-07-21 07:34:14 +0800

    Update bns_home_page.dart

--- bns_home_page.dart
+++ bns_home_page.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: use_key_in_widget_constructors, unnecessary_string_interpolations, unnecessary_to_list_in_spreads, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:printing/printing.dart';
